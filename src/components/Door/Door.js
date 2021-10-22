1df4073bb66355f9ed54c0c8081e29a23d0776da
JekIquin <jci.dev.0@gmail.com>
2021-10-22 12:00:35 -0700

    added images of bunnies

--- src/components/Door/Door.js
+++ src/components/Door/Door.js
@@ -2,6 +2,8 @@
 import PropTypes from 'prop-types';
 import './Door.scss';
 import doorImage from '../../assets/images/Door.png';
+import killerImage from '../../assets/images/killer.png';
+import safeImage from '../../assets/images/safebunny.png';
 
 function Door({doorModel, handleOpenDoor, doorOpened}) {
     const {isOpening, isOpened, isKiller} = doorModel;
@@ -26,6 +28,11 @@ function Door({doorModel, handleOpenDoor, doorOpened}) {
                 className={doorClassName()} 
                 onClick={() => handleOpenDoor(doorModel)}
                 onAnimationEnd={() => doorOpened(doorModel)} 
+            />
+            <img 
+                src={isKiller ? killerImage : safeImage} 
+                alt='' 
+                className='Door__bunny' 
             />
         </div>
         
